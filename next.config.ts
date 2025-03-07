ef681ce86c1dc0ecb698a457d0ed81f157ccf06e
KKangHHee <skh8609@gmail.com>
2025-03-07 15:29:05 +0900

    chore: prettier 정의 및 export 방식 정의

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   /* config options here */
 };
 
